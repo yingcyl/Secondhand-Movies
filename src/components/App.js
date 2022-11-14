d58006d8b30a093b05704ccf2a0c74c8cc39619f
yingcyl <chiayinglow@gmail.com>
2022-11-14 12:46:46 +0000

    Initialize state and set up API call using axios
    -Create two API calls for movie details and list of movie genres
    -Convert genre list to a Map object to easily access genre name based on genre id
    -Set state to API results

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,27 @@ import React from "react";
 import axios from "axios";
 
 class App extends React.Component {
+  state = { movies: [], genre: new Map() };
+
+  componentDidMount() {
+    const term = "budapest";
+    const request1 = axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&query=${term}`
+    );
+    const request2 = axios.get(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+
+    axios.all([request1, request2]).then(
+      axios.spread((...res) => {
+        const res1 = res[0];
+        const res2 = res[1];
+        const genres = new Map(res2.data.genres.map((i) => [i.id, i.name]));
+        this.setState({ movies: res1.data.results, genres: genres });
+      })
+    );
+  }
+
   render() {
     return <div>Hi</div>;
   }
