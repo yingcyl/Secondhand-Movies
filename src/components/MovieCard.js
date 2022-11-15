cb786b1213781e4ef94d8399c409c89bbe0f4775
yingcyl <chiayinglow@gmail.com>
2022-11-15 14:27:12 +0000

    Assign random stock amount and price to each movie
    -Assign prices based on movie's release date
    -Assign random stock numbers based on movie id

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,13 +4,35 @@ import "./MovieCard.css";
 
 class MovieCard extends React.Component {
   render() {
-    const { poster_path, title, release_date, id, genre_ids } =
+    const { poster_path, title, release_date, id, genre_ids, popularity } =
       this.props.movie;
     const genres = genre_ids.map((genre_id) => {
       const genreName = this.props.genre.get(genre_id);
       // lists within lists will need unique keys as well
       return <ul key={genre_id}>{genreName}</ul>;
     });
+    console.log(release_date.substring(0, 4));
+    const price = function moviePrice() {
+      let moviePrice;
+      if (parseInt(release_date.substring(0, 4)) < 2010) {
+        moviePrice = "£5.99";
+        return <ul>{moviePrice}</ul>;
+      } else {
+        moviePrice = "£8.99";
+        return <ul>{moviePrice}</ul>;
+      }
+    };
+
+    const stock = function movieStock() {
+      let movieStock;
+      if (id > 50000) {
+        movieStock = 10;
+        return <ul>In stock: {movieStock}</ul>;
+      } else {
+        movieStock = 6;
+        return <ul>In stock: {movieStock}</ul>;
+      }
+    };
 
     return (
       <div className="movie-card">
@@ -22,6 +44,8 @@ class MovieCard extends React.Component {
             <ul>{title}</ul>
             <ul>{release_date}</ul>
             {genres}
+            {price()}
+            {stock()}
           </div>
         </li>
       </div>
