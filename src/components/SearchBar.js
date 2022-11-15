9c6fc9e5879d3e5831781f1d173fb299fde28167
yingcyl <chiayinglow@gmail.com>
2022-11-15 13:46:38 +0000

    Prevent default form submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,21 +3,20 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  //   onFormSubmit = (e) => {
-  //     e.preventDefault();
-  //     //   referencing the prop passed in the searchbar component in the App.js file. every time the form is submitted, this function will be called and it will call the onsearchsubmit function in the app file which will take the latest this.state.term and run the function
-  //     this.props.onSubmit(this.state.term);
-  //   };
+  onFormSubmit = (e) => {
+    e.preventDefault();
+  };
 
   onInputChange = (e) => {
     this.setState({ term: e.target.value });
+    //   referencing the prop passed in the searchbar component in the App.js file. every time the form is submitted, this function will be called and it will call the onsearchsubmit function in the app file which will take the latest this.state.term and run the function
     this.props.onSubmit(this.state.term);
   };
 
   render() {
     return (
       <div className="ui category search">
-        <form>
+        <form onSubmit={this.onFormSubmit}>
           <div className="ui icon input">
             <input
               className="prompt"
