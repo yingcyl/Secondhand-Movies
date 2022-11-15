ff0c542244185e44edfd9ee75ea84a3d23da5ee3
yingcyl <chiayinglow@gmail.com>
2022-11-15 13:40:52 +0000

    Edit conditional statement for movies with missing information
    -Exclude movies with no release dates and genre list from results

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -8,7 +8,11 @@ const MovieList = (props) => {
     //   const genreName = props.genres.get(id);
     //   return genreName;
     // });
-    if (movie.poster_path === null) {
+    if (
+      movie.poster_path === null ||
+      movie.release_date == "" ||
+      movie.genre_ids.length == 0
+    ) {
       return false;
     } else {
       return <MovieCard key={movie.id} movie={movie} genre={props.genres} />;
