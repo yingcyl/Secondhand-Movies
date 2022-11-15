6533c7f7420d7a143d8da9a67fa4ac79344c9ded
yingcyl <chiayinglow@gmail.com>
2022-11-15 11:06:03 +0000

    Create conditional statement to exclude movies with no posters

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,11 +3,16 @@ import MovieCard from "./MovieCard";
 
 const MovieList = (props) => {
   const movies = props.movies.map((movie) => {
+    console.log(movie);
     // const genres = movie.genre_ids.map((id) => {
     //   const genreName = props.genres.get(id);
     //   return genreName;
     // });
-    return <MovieCard key={movie.id} movie={movie} genre={props.genres} />;
+    if (movie.poster_path === null) {
+      return false;
+    } else {
+      return <MovieCard key={movie.id} movie={movie} genre={props.genres} />;
+    }
   });
   return <div>{movies}</div>;
 };
